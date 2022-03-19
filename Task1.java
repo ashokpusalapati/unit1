d09671d16076df9a7e0d42e4d7b093a534c5e813
ashokpusalapati <101887360+ashokpusalapati@users.noreply.github.com>
2022-03-19 12:16:29 +0530

    Thread using different Thread Pools - Task1

--- Task1.java
+++ Task1.java
@@ -1,14 +1,20 @@
-/*
-Create a Thread using Thread class and print the Thread name
-*/
 class MyThread extends Thread{
 	public void run(){
-		System.out.println(Thread.currentThread().getName());
+		System.out.println(Thread.currentThread().getName()+":\tHello All");
 	}
 }
 class Task1{
 	public static void main(String[] args) {
-		MyThread t=new MyThread();
-		t.start();
+		MyThread m1=new MyThread();
+		m1.start();
+		MyThread m2=new MyThread();
+		m2.start();
+		MyThread m3=new MyThread();
+		m3.start();
+		MyThread m4=new MyThread();
+		m4.start();
+		MyThread m5=new MyThread();
+		m5.start();
+
 	}
 }
