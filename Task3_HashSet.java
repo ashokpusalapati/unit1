3d0a1127b472ba457d7621b5ff22d3fafe693321
ashokpusalapati <101887360+ashokpusalapati@users.noreply.github.com>
2022-03-21 15:52:02 +0530

    Collection Framework on scenario based qtns- Task3

---
+++ Task3_HashSet.java
@@ -0,0 +1,34 @@
+/*
+write a java program to store group of integers[order of insertion  elemetns does not matter and store unique elements](Hint- use HashSet)
+*/
+import java.util.HashSet;
+import java.util.Scanner;
+class Task3_HashSet {
+    public static void main(String args[]){
+        int ch;
+        Scanner scan=new Scanner(System.in);
+        HashSet<Integer> h=new HashSet<Integer>();
+
+        do{
+        	System.out.println("1 for insertion,\t2 for display,\t0 for exit\nEnter your choice: ");
+        	ch=scan.nextInt();
+        	switch(ch){
+        		case 1:
+        			System.out.print("Enter the value to insert: ");
+        			h.add(scan.nextInt());
+        			break;
+        		case 2:
+        			System.out.println("\n-------\n"+h+"\n--------\n");
+        			break;
+        		case 0:
+        			ch=0;
+        			break;
+        		default:
+        			System.out.println("\nPlease enter valid input...");
+
+        	}
+        }while(ch!=0);
+    }
+}
+
+
